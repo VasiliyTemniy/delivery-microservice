13e45cd1037fd08d7c9a3ead3c47a150a6d47bbe
VasiliyTemniy <flash_er@mail.ru>
2024-01-16 20:17:43 +0300

    Project init

---
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/DeliveryMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.vasiliytemniy.deliverymicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryMicroserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeliveryMicroserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/vasiliytemniy/deliverymicroservice/DeliveryMicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vasiliytemniy.deliverymicroservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliveryMicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
