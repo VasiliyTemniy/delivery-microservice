5e8c008299bc15e26df79dd2b90ba572b0d30b69
VasiliyTemniy <flash_er@mail.ru>
2024-01-23 14:42:02 +0300

    rename migrations following convention rules

--- src/main/resources/db/migration/V1_0_2__order_tracking.sql
+++ src/main/resources/db/migration/V1_0_2__order_tracking.sql
@@ -16,5 +16,19 @@ CREATE TABLE IF NOT EXISTS delivery.order_trackings
     delivered_at           TIMESTAMP WITH TIME ZONE,
 
     created_at      TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP
+    updated_at      TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_order_point_pair UNIQUE (order_id, point_number),
 );
+
+-- Set the default value for point_number based on the existing records for the same order_id
+ALTER TABLE delivery.order_trackings
+    ALTER COLUMN point_number SET DEFAULT (
+        SELECT COALESCE(MAX(subquery.row_number), 0)
+        FROM (
+            SELECT ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY id) AS row_number
+            FROM delivery.order_trackings
+        ) AS subquery
+        WHERE subquery.order_id = order_trackings.order_id
+    );
+
+CREATE INDEX IF NOT EXISTS order_trackings_order_id_point_number_idx ON delivery.order_trackings (order_id, point_number);
