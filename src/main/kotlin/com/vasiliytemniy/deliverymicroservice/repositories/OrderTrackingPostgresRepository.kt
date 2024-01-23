c757011feb76254906684828fd86e9ec4beccce0
VasiliyTemniy <flash_er@mail.ru>
2024-01-23 14:59:25 +0300

    add methods

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingPostgresRepository.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingPostgresRepository.kt
@@ -6,20 +6,30 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Flux
 
 
 @Repository
 interface OrderTrackingPostgresRepository {
 
     @Query("SELECT * FROM order_tracking WHERE order_id = :orderId")
-    suspend fun findByOrderId(orderId: Long, pageable: Pageable): Flux<OrderTracking>
+    suspend fun findByOrderId(orderId: Long, pageable: Pageable): Page<OrderTracking>
 
     @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId")
-    suspend fun findByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+    suspend fun findByCarrierId(carrierId: Long, pageable: Pageable): Page<OrderTracking>
 
-//    suspend fun findByOrderId(orderId: Long, pageable: Pageable): Page<OrderTracking>
-//
-//    suspend fun findByCarrierId(carrierId: Long, pageable: Pageable): Page<OrderTracking>
+    @Query("SELECT * FROM order_tracking WHERE order_id = :orderId")
+    fun findByOrderIdFlow(orderId: Long, pageable: Pageable): Flow<OrderTracking>
+
+    @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId")
+    fun findByCarrierIdFlow(carrierId: Long, pageable: Pageable): Flow<OrderTracking>
+
+    @Query("SELECT * FROM order_tracking WHERE order_id = :orderId ORDER BY point_number DESC LIMIT 1")
+    suspend fun findLastByOrderId(orderId: Long): OrderTracking
+
+    @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId AND delivered_at IS NULL")
+    suspend fun findActiveByCarrierId(carrierId: Long, pageable: Pageable): Page<OrderTracking>
+
+    @Query("UPDATE order_tracking SET status = :status, delivered_at = :deliveredAt WHERE order_id = :orderId, point_number = :pointNumber")
+    suspend fun setOrderTrackingStatus(orderId: Long, pointNumber: Int, status: String, deliveredAt: String?): OrderTracking
 
 }
