cc108d511223b839d64cfc512f24da53a50ecd64
VasiliyTemniy <flash_er@mail.ru>
2024-01-23 14:59:43 +0300

    add methods + implementations

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/OrderTrackingService.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/OrderTrackingService.kt
@@ -5,6 +5,7 @@ import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
 import com.vasiliytemniy.deliverymicroservice.dto.GetOrderTrackingsByOrderIdDto
 import com.vasiliytemniy.deliverymicroservice.dto.GetOrderTrackingsByCarrierIdDto
 import com.vasiliytemniy.deliverymicroservice.dto.SetOrderTrackingStatusDto
+import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 
 
@@ -13,12 +14,17 @@ interface OrderTrackingService {
 
     suspend fun createOrderTracking(orderTracking: OrderTracking): OrderTracking
 
-//    suspend fun getOrderTrackingsByOrderId(requestDto: GetOrderTrackingsByOrderIdDto): Page<OrderTracking>
-//
-//    suspend fun getOrderTrackingsByCarrierId(requestDto: GetOrderTrackingsByCarrierIdDto): Page<OrderTracking>
-//
-//    suspend fun updateOrderTracking(orderTracking: OrderTracking): OrderTracking
-//
-//    suspend fun setOrderTrackingStatus(requestDto: SetOrderTrackingStatusDto): OrderTracking
+    suspend fun getOrderTrackingsByOrderId(requestDto: GetOrderTrackingsByOrderIdDto): Page<OrderTracking>
 
+    suspend fun getOrderTrackingsByCarrierId(requestDto: GetOrderTrackingsByCarrierIdDto): Page<OrderTracking>
+
+    fun getOrderTrackingsByOrderIdFlow(requestDto: GetOrderTrackingsByOrderIdDto): Flow<OrderTracking>
+
+    fun getOrderTrackingsByCarrierIdFlow(requestDto: GetOrderTrackingsByCarrierIdDto): Flow<OrderTracking>
+
+    suspend fun getLastOrderTrackingByOrderId(orderId: Long): OrderTracking?
+
+    suspend fun getActiveOrderTrackingsByCarrierId(requestDto: GetOrderTrackingsByCarrierIdDto): Page<OrderTracking>
+
+    suspend fun setOrderTrackingStatus(requestDto: SetOrderTrackingStatusDto): OrderTracking
 }
--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/OrderTrackingServiceImpl.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/OrderTrackingServiceImpl.kt
@@ -1,9 +1,14 @@
 package com.vasiliytemniy.deliverymicroservice.services
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import com.vasiliytemniy.deliverymicroservice.dto.GetOrderTrackingsByCarrierIdDto
+import com.vasiliytemniy.deliverymicroservice.dto.GetOrderTrackingsByOrderIdDto
+import com.vasiliytemniy.deliverymicroservice.dto.SetOrderTrackingStatusDto
 import com.vasiliytemniy.deliverymicroservice.repositories.OrderTrackingRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
+import org.springframework.data.domain.Page
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import javax.validation.Valid
@@ -17,6 +22,44 @@ class OrderTrackingServiceImpl(
     override suspend fun createOrderTracking(@Valid orderTracking: OrderTracking): OrderTracking =
         withContext(Dispatchers.IO) {
             orderTrackingRepository.save(orderTracking)
+        }
+
+    @Transactional(readOnly = true)
+    override suspend fun getOrderTrackingsByOrderId(requestDto: GetOrderTrackingsByOrderIdDto): Page<OrderTracking> =
+        withContext(Dispatchers.IO) {
+            orderTrackingRepository.findByOrderId(requestDto.orderId, requestDto.pageable)
+        }
+
+    @Transactional(readOnly = true)
+    override suspend fun getOrderTrackingsByCarrierId(requestDto: GetOrderTrackingsByCarrierIdDto): Page<OrderTracking> =
+        withContext(Dispatchers.IO) {
+            orderTrackingRepository.findByCarrierId(requestDto.carrierId, requestDto.pageable)
+        }
+
+    @Transactional(readOnly = true)
+    override fun getOrderTrackingsByOrderIdFlow(requestDto: GetOrderTrackingsByOrderIdDto): Flow<OrderTracking> =
+        orderTrackingRepository.findByOrderIdFlow(requestDto.orderId, requestDto.pageable)
+
+    @Transactional(readOnly = true)
+    override fun getOrderTrackingsByCarrierIdFlow(requestDto: GetOrderTrackingsByCarrierIdDto): Flow<OrderTracking> =
+        orderTrackingRepository.findByCarrierIdFlow(requestDto.carrierId, requestDto.pageable)
+
+    @Transactional(readOnly = true)
+    override suspend fun getLastOrderTrackingByOrderId(orderId: Long): OrderTracking? =
+        withContext(Dispatchers.IO) {
+            orderTrackingRepository.findLastByOrderId(orderId)
+        }
+
+    @Transactional(readOnly = true)
+    override suspend fun getActiveOrderTrackingsByCarrierId(requestDto: GetOrderTrackingsByCarrierIdDto): Page<OrderTracking> =
+        withContext(Dispatchers.IO) {
+            orderTrackingRepository.findActiveByCarrierId(requestDto.carrierId, requestDto.pageable)
+        }
+
+    @Transactional
+    override suspend fun setOrderTrackingStatus(requestDto: SetOrderTrackingStatusDto): OrderTracking =
+        withContext(Dispatchers.IO) {
+            orderTrackingRepository.setOrderTrackingStatus(requestDto.orderId, requestDto.pointNumber, requestDto.status, requestDto.deliveredAt)
         }
 
     companion object {
