6810fb99cad805b8939d93c467e5b905c69d0136
VasiliyTemniy <flash_er@mail.ru>
2024-01-23 17:15:16 +0300

    rename OrderTrackingRepository to OrderTrackingCoroutineRepository; move flow-returning methods to Coroutine repository

---
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingCoroutineRepository.kt
@@ -0,0 +1,20 @@
+package com.vasiliytemniy.deliverymicroservice.repositories
+
+import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.domain.Pageable
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface OrderTrackingCoroutineRepository: CoroutineCrudRepository<OrderTracking, Long> {
+
+    @Query("SELECT * FROM order_tracking WHERE order_id = :orderId")
+    fun findByOrderIdFlow(orderId: Long, pageable: Pageable): Flow<OrderTracking>
+
+    @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId")
+    fun findByCarrierIdFlow(carrierId: Long, pageable: Pageable): Flow<OrderTracking>
+
+}
