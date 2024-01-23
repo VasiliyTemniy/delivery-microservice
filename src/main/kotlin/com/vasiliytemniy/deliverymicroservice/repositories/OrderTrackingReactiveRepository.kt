74a6da00ab8ad626bab08348e4ae2b5b47367d01
VasiliyTemniy <flash_er@mail.ru>
2024-01-23 17:15:00 +0300

    rename OrderTrackingPostgresRepository to OrderTrackingReactiveRepository; move flow-returning methods to Coroutine repository

---
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveRepository.kt
@@ -0,0 +1,30 @@
+package com.vasiliytemniy.deliverymicroservice.repositories
+
+import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import org.springframework.data.domain.Pageable
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+
+@Repository
+interface OrderTrackingReactiveRepository: ReactiveCrudRepository<OrderTracking, Long> {
+
+//    @Query("SELECT * FROM order_tracking WHERE order_id = :orderId")
+    fun findByOrderId(orderId: Long, pageable: Pageable): Flux<OrderTracking>
+
+//    @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId")
+    fun findByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+
+    @Query("SELECT * FROM order_tracking WHERE order_id = :orderId ORDER BY point_number DESC LIMIT 1")
+    fun findLastByOrderId(orderId: Long): Mono<OrderTracking>
+
+    @Query("SELECT * FROM order_tracking WHERE carrier_id = :carrierId AND delivered_at IS NULL")
+    fun findActiveByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+
+    @Query("UPDATE order_tracking SET  status = :status, delivered_at = :deliveredAt WHERE order_id = :orderId, point_number = :pointNumber")
+    fun setOrderTrackingStatus(orderId: Long, pointNumber: Int, status: String, deliveredAt: String?): Mono<OrderTracking>
+
+}
