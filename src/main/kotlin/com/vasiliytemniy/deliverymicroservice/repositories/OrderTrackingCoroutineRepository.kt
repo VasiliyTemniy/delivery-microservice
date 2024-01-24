c2681075cee707cc93bf2de5f8c0b8ffa4a5330d
VasiliyTemniy <flash_er@mail.ru>
2024-01-24 17:02:21 +0300

    implement all methods for both repositories for test purposes

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingCoroutineRepository.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingCoroutineRepository.kt
@@ -1,20 +1,68 @@
 package com.vasiliytemniy.deliverymicroservice.repositories
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import com.vasiliytemniy.deliverymicroservice.repositories.LongSqlQueries.Companion.ORDER_TRACKING_UPDATE_SQL_QUERY
+import com.vasiliytemniy.deliverymicroservice.repositories.LongSqlQueries.Companion.SET_ORDER_TRACKING_STATUS_SQL_QUERY
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 
 @Repository
-interface OrderTrackingCoroutineRepository: CoroutineCrudRepository<OrderTracking, Long> {
+interface OrderTrackingCoroutineRepository : CoroutineCrudRepository<OrderTracking, Long> {
 
-    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
-    fun findByOrderIdFlow(orderId: Long, pageable: Pageable): Flow<OrderTracking>
+    //    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
+    fun findPageByOrderId(orderId: Long, pageable: Pageable): Flow<OrderTracking>
 
-    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
-    fun findByCarrierIdFlow(carrierId: Long, pageable: Pageable): Flow<OrderTracking>
+    //    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
+    fun findAllByOrderId(orderId: Long): Flow<OrderTracking>
+
+    //    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
+    fun findPageByCarrierId(carrierId: Long, pageable: Pageable): Flow<OrderTracking>
+
+    //    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
+    fun findAllByCarrierId(carrierId: Long): Flow<OrderTracking>
+
+    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId ORDER BY point_number DESC LIMIT 1")
+    suspend fun findLastByOrderId(orderId: Long): OrderTracking
+
+    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId AND delivered_at IS NULL")
+    fun findPageActiveByCarrierId(carrierId: Long, pageable: Pageable): Flow<OrderTracking>
+
+    @Query(SET_ORDER_TRACKING_STATUS_SQL_QUERY)
+    suspend fun setOrderTrackingStatus(
+        orderId: Long,
+        pointNumber: Int,
+        status: String,
+        deliveredAt: String?
+    ): OrderTracking
+
+    @Query(ORDER_TRACKING_UPDATE_SQL_QUERY)
+    fun updateOrderTracking(
+        orderId: Long,
+        pointNumber: Int,
+        fromFacilityId: Long,
+        destinationId: Long,
+        destinationType: String,
+        carrierId: Long,
+        status: String,
+        deliveryCost: Int,
+        currency: String,
+        currencyDecimalMultiplier: Int,
+        massControlValue: Int?,
+        massMeasure: String?,
+        estimatedDeliveryAt: String?,
+        deliveredAt: String?
+    ): OrderTracking
+
+    @Query("DELETE FROM order_trackings WHERE order_id = :orderId")
+    suspend fun deleteAllByOrderId(orderId: Long): Unit
+
+    @Query("DELETE FROM order_trackings WHERE order_id = :orderId AND point_number = :pointNumber")
+    suspend fun deleteByOrderTrackingIdentifier(orderId: Long, pointNumber: Int): Unit
 
 }
--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveRepository.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveRepository.kt
@@ -1,6 +1,8 @@
 package com.vasiliytemniy.deliverymicroservice.repositories
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import com.vasiliytemniy.deliverymicroservice.repositories.LongSqlQueries.Companion.ORDER_TRACKING_UPDATE_SQL_QUERY
+import com.vasiliytemniy.deliverymicroservice.repositories.LongSqlQueries.Companion.SET_ORDER_TRACKING_STATUS_SQL_QUERY
 import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
@@ -10,21 +12,56 @@ import reactor.core.publisher.Mono
 
 
 @Repository
-interface OrderTrackingReactiveRepository: ReactiveCrudRepository<OrderTracking, Long> {
+interface OrderTrackingReactiveRepository : ReactiveCrudRepository<OrderTracking, Long> {
 
-//    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
-    fun findByOrderId(orderId: Long, pageable: Pageable): Flux<OrderTracking>
+    //    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
+    fun findPageByOrderId(orderId: Long, pageable: Pageable): Flux<OrderTracking>
 
-//    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
-    fun findByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+    //    @Query("SELECT * FROM order_trackings WHERE order_id = :orderId")
+    fun findAllByOrderId(orderId: Long): Flux<OrderTracking>
+
+    //    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
+    fun findPageByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+
+    //    @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId")
+    fun findAllByCarrierId(carrierId: Long): Flux<OrderTracking>
 
     @Query("SELECT * FROM order_trackings WHERE order_id = :orderId ORDER BY point_number DESC LIMIT 1")
     fun findLastByOrderId(orderId: Long): Mono<OrderTracking>
 
     @Query("SELECT * FROM order_trackings WHERE carrier_id = :carrierId AND delivered_at IS NULL")
-    fun findActiveByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+    fun findPageActiveByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
 
-    @Query("UPDATE order_trackings SET status = :status, delivered_at = :deliveredAt WHERE order_id = :orderId, point_number = :pointNumber")
-    fun setOrderTrackingStatus(orderId: Long, pointNumber: Int, status: String, deliveredAt: String?): Mono<OrderTracking>
+    @Query(SET_ORDER_TRACKING_STATUS_SQL_QUERY)
+    fun setOrderTrackingStatus(
+        orderId: Long,
+        pointNumber: Int,
+        status: String,
+        deliveredAt: String?
+    ): Mono<OrderTracking>
+
+    @Query(ORDER_TRACKING_UPDATE_SQL_QUERY)
+    fun updateOrderTracking(
+        orderId: Long,
+        pointNumber: Int,
+        fromFacilityId: Long,
+        destinationId: Long,
+        destinationType: String,
+        carrierId: Long,
+        status: String,
+        deliveryCost: Int,
+        currency: String,
+        currencyDecimalMultiplier: Int,
+        massControlValue: Int?,
+        massMeasure: String?,
+        estimatedDeliveryAt: String?,
+        deliveredAt: String?
+    ): Mono<OrderTracking>
+
+    @Query("DELETE FROM order_trackings WHERE order_id = :orderId")
+    fun deleteAllByOrderId(orderId: Long): Mono<Void>
+
+    @Query("DELETE FROM order_trackings WHERE order_id = :orderId AND point_number = :pointNumber")
+    fun deleteByOrderTrackingIdentifier(orderId: Long, pointNumber: Int): Mono<Void>
 
 }
