fd03e2ec6da692ffb8a700019c3beaeff25975d9
VasiliyTemniy <flash_er@mail.ru>
2024-01-27 21:13:25 +0300

    add util to parse nullable date

---
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/utils/StringToDateParser.kt
@@ -0,0 +1,13 @@
+package com.vasiliytemniy.deliverymicroservice.utils
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+fun parseOptionalDate(request: String?): LocalDateTime? {
+    return if (request == null || request == "" || request == "null") {
+        null
+    } else {
+        LocalDateTime.parse(request, DateTimeFormatter.ISO_DATE)
+    }
+}
