7679254f5919a6f9eff6fcacce6a5a5f9aee65f8
VasiliyTemniy <flash_er@mail.ru>
2024-01-27 21:14:42 +0300

    upd

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/SqlQueries.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/SqlQueries.kt
@@ -3,51 +3,15 @@ package com.vasiliytemniy.deliverymicroservice.repositories
 class SqlQueries {
 
     companion object {
-        const val FIND_PAGE_BY_ORDER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
-            WHERE order_id = :orderId
-            ORDER BY point_number ASC
-            LIMIT :pageable.pageSize
-            OFFSET :pageable.offset
-        """
-
-        const val FIND_ALL_BY_ORDER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
-            WHERE order_id = :orderId
-            ORDER BY point_number ASC
-        """
-
         const val FIND_LAST_BY_ORDER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
+            SELECT * FROM delivery.order_trackings
             WHERE order_id = :orderId
             ORDER BY point_number DESC
             LIMIT 1
         """
 
-        const val FIND_PAGE_BY_CARRIER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
-            WHERE carrier_id = :carrierId
-            ORDER BY point_number ASC
-            LIMIT :pageable.pageSize
-            OFFSET :pageable.offset
-        """
-
-        const val FIND_ALL_BY_CARRIER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
-            WHERE carrier_id = :carrierId
-            ORDER BY point_number ASC
-        """
-
-        const val FIND_PAGE_ACTIVE_BY_CARRIER_ID_SQL_QUERY = """
-            SELECT * FROM order_trackings
-            WHERE carrier_id = :carrierId AND delivered_at IS NULL
-            ORDER BY point_number ASC
-            LIMIT :pageable.pageSize
-            OFFSET :pageable.offset
-        """
-
         const val UPDATE_ORDER_TRACKING_SQL_QUERY = """
-            UPDATE order_trackings
+            UPDATE delivery.order_trackings
             SET
                 from_facility_id = :fromFacilityId,
                 destination_id = :destinationId,
@@ -62,32 +26,48 @@ class SqlQueries {
                 estimated_delivery_at = :estimatedDeliveryAt,
                 delivered_at = :deliveredAt
             WHERE
-                order_id = :orderId, point_number = :pointNumber
+                order_id = :orderId AND point_number = :pointNumber
             RETURNING *
         """
 
         const val SET_ORDER_TRACKING_STATUS_SQL_QUERY = """
-            UPDATE order_trackings
+            UPDATE delivery.order_trackings
             SET
                 status = :status,
                 delivered_at = :deliveredAt
             WHERE
-                order_id = :orderId, point_number = :pointNumber
+                order_id = :orderId AND point_number = :pointNumber
             RETURNING *
         """
 
         const val DELETE_ALL_ORDER_TRACKINGS_BY_ORDER_ID_SQL_QUERY = """
-            DELETE FROM order_trackings
+            DELETE FROM delivery.order_trackings
             WHERE
                 order_id = :orderId
             RETURNING *
         """
 
         const val DELETE_ORDER_TRACKING_SQL_QUERY = """
-            DELETE FROM order_trackings
+            DELETE FROM delivery.order_trackings
             WHERE
-                order_id = :orderId, point_number = :pointNumber
+                order_id = :orderId AND point_number = :pointNumber
             RETURNING *
+        """
+
+        const val SELECT_COUNT_SQL_QUERY = """
+            SELECT count(id) AS total FROM delivery.order_trackings
+        """
+
+        const val SELECT_COUNT_BY_ORDER_ID_SQL_QUERY = """
+            SELECT count(id) AS total FROM delivery.order_trackings
+            WHERE
+                order_id = :orderId
+        """
+
+        const val SELECT_COUNT_BY_CARRIER_ID_SQL_QUERY = """
+            SELECT count(id) AS total FROM delivery.order_trackings
+            WHERE
+                carrier_id = :carrierId
         """
     }
 
