1293cad08a602f87c52f9300489fe4d39a019394
VasiliyTemniy <flash_er@mail.ru>
2024-01-27 21:14:38 +0300

    upd

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveRepository.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveRepository.kt
@@ -1,47 +1,35 @@
 package com.vasiliytemniy.deliverymicroservice.repositories
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.DELETE_ALL_ORDER_TRACKINGS_BY_ORDER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.DELETE_ORDER_TRACKING_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_ALL_BY_CARRIER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_ALL_BY_ORDER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_LAST_BY_ORDER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_PAGE_ACTIVE_BY_CARRIER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_PAGE_BY_CARRIER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.FIND_PAGE_BY_ORDER_ID_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.SET_ORDER_TRACKING_STATUS_SQL_QUERY
-import com.vasiliytemniy.deliverymicroservice.repositories.SqlQueries.Companion.UPDATE_ORDER_TRACKING_SQL_QUERY
 import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 
 @Repository
 interface OrderTrackingReactiveRepository : ReactiveCrudRepository<OrderTracking, Long> {
 
-    @Query(FIND_PAGE_BY_ORDER_ID_SQL_QUERY)
     fun findPageByOrderId(orderId: Long, pageable: Pageable): Flux<OrderTracking>
 
-    @Query(FIND_ALL_BY_ORDER_ID_SQL_QUERY)
     fun findAllByOrderId(orderId: Long): Flux<OrderTracking>
 
-    @Query(FIND_LAST_BY_ORDER_ID_SQL_QUERY)
+    @Query(SqlQueries.FIND_LAST_BY_ORDER_ID_SQL_QUERY)
     fun findLastByOrderId(orderId: Long): Mono<OrderTracking>
 
-    @Query(FIND_PAGE_BY_CARRIER_ID_SQL_QUERY)
     fun findPageByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
 
-    @Query(FIND_ALL_BY_CARRIER_ID_SQL_QUERY)
     fun findAllByCarrierId(carrierId: Long): Flux<OrderTracking>
 
-    @Query(FIND_PAGE_ACTIVE_BY_CARRIER_ID_SQL_QUERY)
-    fun findPageActiveByCarrierId(carrierId: Long, pageable: Pageable): Flux<OrderTracking>
+    // A tricky workaround to filter active order trackings with deliveredAt without custom impl
+    fun findPageByCarrierIdAndDeliveredAt(carrierId: Long, deliveredAt: LocalDateTime?, pageable: Pageable): Flux<OrderTracking>
+    fun findAllByCarrierIdAndDeliveredAt(carrierId: Long, deliveredAt: LocalDateTime?): Flux<OrderTracking>
 
-    @Query(UPDATE_ORDER_TRACKING_SQL_QUERY)
-    fun updateOrderTracking(
+    @Query(SqlQueries.UPDATE_ORDER_TRACKING_SQL_QUERY)
+    fun update(
         orderId: Long,
         pointNumber: Int,
         fromFacilityId: Long,
@@ -54,22 +42,22 @@ interface OrderTrackingReactiveRepository : ReactiveCrudRepository<OrderTracking
         currencyDecimalMultiplier: Int,
         massControlValue: Int?,
         massMeasure: String?,
-        estimatedDeliveryAt: String?,
-        deliveredAt: String?
+        estimatedDeliveryAt: LocalDateTime?,
+        deliveredAt: LocalDateTime?
     ): Mono<OrderTracking>
 
-    @Query(SET_ORDER_TRACKING_STATUS_SQL_QUERY)
-    fun setOrderTrackingStatus(
+    @Query(SqlQueries.SET_ORDER_TRACKING_STATUS_SQL_QUERY)
+    fun setStatus(
         orderId: Long,
         pointNumber: Int,
         status: String,
-        deliveredAt: String?
+        deliveredAt: LocalDateTime?
     ): Mono<OrderTracking>
 
-    @Query(DELETE_ALL_ORDER_TRACKINGS_BY_ORDER_ID_SQL_QUERY)
+    @Query(SqlQueries.DELETE_ALL_ORDER_TRACKINGS_BY_ORDER_ID_SQL_QUERY)
     fun deleteAllByOrderId(orderId: Long): Flux<OrderTracking>
 
-    @Query(DELETE_ORDER_TRACKING_SQL_QUERY)
-    fun deleteByOrderTrackingIdentifier(orderId: Long, pointNumber: Int): Mono<OrderTracking>
+    @Query(SqlQueries.DELETE_ORDER_TRACKING_SQL_QUERY)
+    fun deleteByExternalId(orderId: Long, pointNumber: Int): Mono<OrderTracking>
 
 }
