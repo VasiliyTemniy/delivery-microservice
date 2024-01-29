ae8c310b74132a1b1fc7d9dd2f26b6419f9aadf8
VasiliyTemniy <flash_er@mail.ru>
2024-01-29 13:57:47 +0300

    move some 4 methods to custom impl; combine them in 2; add countBy... methods

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveCustomRepository.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveCustomRepository.kt
@@ -1,13 +1,24 @@
 package com.vasiliytemniy.deliverymicroservice.repositories
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 
 @Repository
 interface OrderTrackingReactiveCustomRepository {
 
+    fun findPageByCarrierId(carrierId: String, pageable: Pageable, filterActive: Boolean): Flux<OrderTracking>
+
+    fun findAllByCarrierId(carrierId: String, filterActive: Boolean): Flux<OrderTracking>
+
     fun setPointNumber(orderId: String, fromPointNumber: Int, toPointNumber: Int): Mono<OrderTracking>
+
+    fun countByOrderId(orderId: String): Mono<Long>
+
+    fun countByCarrierId(carrierId: String, filterActive: Boolean): Mono<Long>
 
 }
--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveCustomRepositoryImpl.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/repositories/OrderTrackingReactiveCustomRepositoryImpl.kt
@@ -2,14 +2,48 @@ package com.vasiliytemniy.deliverymicroservice.repositories
 
 import com.vasiliytemniy.deliverymicroservice.domain.OrderTracking
 import com.vasiliytemniy.deliverymicroservice.domain.of
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitFirst
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.relational.core.query.Criteria
+import org.springframework.data.relational.core.query.Query
+import org.springframework.data.relational.core.query.isEqual
 import org.springframework.r2dbc.core.DatabaseClient
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 class OrderTrackingReactiveCustomRepositoryImpl(
     val template: R2dbcEntityTemplate,
     val databaseClient: DatabaseClient
 ): OrderTrackingReactiveCustomRepository {
+
+    override fun findPageByCarrierId(
+        carrierId: String,
+        pageable: Pageable,
+        filterActive: Boolean
+    ): Flux<OrderTracking> {
+        val query = if (filterActive)
+            Query.query(Criteria.where("carrier_id").isEqual(carrierId))
+        else
+            Query.query(Criteria.where("carrier_id").isEqual(carrierId).and("delivered_at").isNull())
+
+        return template.select(query.with(pageable), OrderTracking::class.java)
+    }
+
+    override fun findAllByCarrierId(carrierId: String, filterActive: Boolean): Flux<OrderTracking> {
+        val query = if (filterActive)
+            Query.query(Criteria.where("carrier_id").isEqual(carrierId))
+        else
+            Query.query(Criteria.where("carrier_id").isEqual(carrierId).and("delivered_at").isNull())
+
+        return template.select(query, OrderTracking::class.java)
+    }
 
     override fun setPointNumber(orderId: String, fromPointNumber: Int, toPointNumber: Int): Mono<OrderTracking> =
         databaseClient.sql(SqlQueries.SET_POINT_NUMBER_SQL_QUERY)
@@ -19,5 +53,29 @@ class OrderTrackingReactiveCustomRepositoryImpl(
             .fetch()
             .first()
             .flatMap { Mono.just(OrderTracking.of(it)) }
+
+    override fun countByOrderId(orderId: String): Mono<Long> =
+        databaseClient.sql(SqlQueries.SELECT_COUNT_BY_ORDER_ID_SQL_QUERY)
+            .bind("orderId", orderId)
+            .fetch()
+            .first()
+            .flatMap { Mono.just(it["total"] as Long) }
+
+    override fun countByCarrierId(carrierId: String, filterActive: Boolean): Mono<Long> {
+        val countQuery = if (filterActive)
+            SqlQueries.SELECT_COUNT_ACTIVE_BY_CARRIER_ID_SQL_QUERY
+        else
+            SqlQueries.SELECT_COUNT_BY_CARRIER_ID_SQL_QUERY
+
+        return databaseClient.sql(countQuery)
+            .bind("carrierId", carrierId)
+            .fetch()
+            .first()
+            .flatMap { Mono.just(it["total"] as Long) }
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(OrderTrackingReactiveCustomRepositoryImpl::class.java)
+    }
 
 }
