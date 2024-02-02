38753e053b645d1a02c2d3d72afa3f2d1b587c9f
VasiliyTemniy <flash_er@mail.ru>
2024-02-02 10:38:14 +0300

    preemptive add companion of request to parse GetOrderTrackingsByFiltersDto to exclude Spring Boot auto parser bugs

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/dto/GetOrderTrackingsByFiltersDto.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/dto/GetOrderTrackingsByFiltersDto.kt
@@ -50,3 +50,151 @@ fun GetOrderTrackingsByFiltersDto.Companion.of(request: OrderTracking.GetPageByF
         request.hasMassMeasureFilter,
         PageRequest.of(request.page, request.size)
     )
+
+
+fun GetOrderTrackingsByFiltersDto.Companion.of(request: Any, page: Int, size: Int): GetOrderTrackingsByFiltersDto {
+
+    val invalidRequestPrefix = "Invalid request: getPageOrderTrackingsByFilters:"
+
+    if (request !is LinkedHashMap<*, *>)
+        throw IllegalArgumentException("$invalidRequestPrefix invalid request body$")
+
+    if (
+        request.keys.size > 6
+        || request.keys.size == 0
+    )
+        throw IllegalArgumentException("$invalidRequestPrefix invalid request body")
+
+    val parsedIdFilters = mutableListOf<IdFilterGroup>()
+
+    if (request.contains("idFilters")) {
+        if (request["idFilters"] !is List<*>) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid idFilters")
+        }
+
+        val requestIdFilters = request["idFilters"] as List<*>
+        requestIdFilters.forEach {
+            if (
+                it !is LinkedHashMap<*, *>
+                || !it.keys.containsAll(listOf("type", "id"))
+                || it["type"] !is String
+                || it["id"] !is String
+                || !IdFilterType.isIdFilterType(it["type"] as String)
+            )
+                throw IllegalArgumentException("$invalidRequestPrefix invalid idFilters")
+
+            parsedIdFilters.add(IdFilterGroup(
+                IdFilterType.valueOf(it["type"] as String),
+                it["id"] as String)
+            )
+        }
+    }
+
+    val parsedTimeFilters = mutableListOf<TimeFilterGroup>()
+
+    if (request.contains("timeFilters")) {
+        if (request["timeFilters"] !is List<*>) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid timeFilters")
+        }
+
+        val requestTimeFilters = request["timeFilters"] as List<*>
+        requestTimeFilters.forEach {
+            if (
+                it !is LinkedHashMap<*, *>
+                || !it.keys.containsAll(listOf("type", "from", "to"))
+                || it["type"] !is String
+                || !(it["from"] is String || it["from"] == null)
+                || !(it["to"] is String || it["to"] == null)
+                || !TimeFilterType.isTimeFilterType(it["type"] as String)
+            )
+                throw IllegalArgumentException("$invalidRequestPrefix invalid timeFilters")
+
+            parsedTimeFilters.add(TimeFilterGroup(
+                TimeFilterType.valueOf(it["type"] as String),
+                parseOptionalDate(it["from"] as String?),
+                parseOptionalDate(it["to"] as String?))
+            )
+        }
+    }
+
+    val parsedEitherEqualStatusFilters = mutableListOf<String>()
+
+    if (request.contains("eitherEqualStatusFilters")) {
+        if (request["eitherEqualStatusFilters"] !is List<*>) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid eitherEqualStatusFilters")
+        }
+
+        val requestEitherEqualStatusFilters = request["eitherEqualStatusFilters"] as List<*>
+        requestEitherEqualStatusFilters.forEach {
+            if (
+                it !is String
+            )
+                throw IllegalArgumentException("$invalidRequestPrefix invalid eitherEqualStatusFilters")
+
+            parsedEitherEqualStatusFilters.add(it)
+        }
+    }
+
+    val parsedNeitherEqualStatusFilters = mutableListOf<String>()
+
+    if (request.contains("neitherEqualStatusFilters")) {
+        if (request["neitherEqualStatusFilters"] !is List<*>) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid neitherEqualStatusFilters")
+        }
+
+        val requestNeitherEqualStatusFilters = request["neitherEqualStatusFilters"] as List<*>
+        requestNeitherEqualStatusFilters.forEach {
+            if (
+                it !is String
+            )
+                throw IllegalArgumentException("$invalidRequestPrefix invalid neitherEqualStatusFilters")
+
+            parsedNeitherEqualStatusFilters.add(it)
+        }
+    }
+
+    val parsedNullablesFilters = mutableListOf<NullablesFilterGroup>()
+
+    if (request.contains("nullablesFilters")) {
+        if (request["nullablesFilters"] !is List<*>) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid nullablesFilters")
+        }
+
+        val requestNullablesFilters = request["nullablesFilters"] as List<*>
+        requestNullablesFilters.forEach {
+            if (
+                it !is LinkedHashMap<*, *>
+                || !it.keys.containsAll(listOf("type", "isOrNotNull"))
+                || it["type"] !is String
+                || it["isOrNotNull"] !is Boolean
+                || !NullablesFilterType.isNullablesFilterType(it["type"] as String)
+            )
+                throw IllegalArgumentException("$invalidRequestPrefix invalid nullablesFilters")
+
+            parsedNullablesFilters.add(NullablesFilterGroup(
+                NullablesFilterType.valueOf(it["type"] as String),
+                it["isOrNotNull"] as Boolean)
+            )
+        }
+    }
+
+    var parsedHasMassMeasureFilter = false
+
+    if (request.contains("hasMassMeasureFilter")) {
+        if (request["hasMassMeasureFilter"] !is Boolean) {
+            throw IllegalArgumentException("$invalidRequestPrefix invalid hasMassMeasureFilter")
+        }
+        parsedHasMassMeasureFilter = request["hasMassMeasureFilter"] as Boolean
+    }
+
+
+    return GetOrderTrackingsByFiltersDto(
+        parsedIdFilters,
+        parsedTimeFilters,
+        parsedEitherEqualStatusFilters,
+        parsedNeitherEqualStatusFilters,
+        parsedNullablesFilters,
+        parsedHasMassMeasureFilter,
+        PageRequest.of(page, size)
+    )
+}
