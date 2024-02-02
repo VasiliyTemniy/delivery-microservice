5542f50933bad7f81e721f6b4f82baaa52227880
VasiliyTemniy <flash_er@mail.ru>
2024-02-02 20:53:51 +0300

    fix - add @Service annotation

--- src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/DeliveryMetaServiceImpl.kt
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/services/DeliveryMetaServiceImpl.kt
@@ -2,8 +2,10 @@ package com.vasiliytemniy.deliverymicroservice.services
 
 import com.vasiliytemniy.deliverymicroservice.domain.DeliveryMeta
 import com.vasiliytemniy.deliverymicroservice.dto.CalculateDeliveryMetaDto
+import org.springframework.stereotype.Service
 
 
+@Service
 class DeliveryMetaServiceImpl(): DeliveryMetaService {
 
     override suspend fun calculateDeliveryMeta(requestDto: CalculateDeliveryMetaDto): DeliveryMeta {
