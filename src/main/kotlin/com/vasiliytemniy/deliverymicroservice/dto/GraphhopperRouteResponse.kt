137d207a1651efcdf7f46b2f0a8ffabdf074c0b1
VasiliyTemniy <flash_er@mail.ru>
2024-02-05 21:13:34 +0300

    add Graphhopper api routing response

---
+++ src/main/kotlin/com/vasiliytemniy/deliverymicroservice/dto/GraphhopperRouteResponse.kt
@@ -0,0 +1,47 @@
+package com.vasiliytemniy.deliverymicroservice.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
+data class GraphhopperRouteResponse(
+    val paths: List<GraphhopperRouteResponsePath>,
+    val info: GraphhopperRouteResponseInfo
+)
+
+// Only distance is needed actually, so other fields are omitted
+data class GraphhopperRouteResponsePath(
+    val distance: Double,
+    val time: Int,
+    val ascend: Double,
+    val descend: Double,
+//    val points: String, // actual format depends on points_encoded - manual deserialize needed
+//    @JsonProperty("snapped_waypoints")
+//    val snappedWaypoints: String, // actual format depends on points_encoded - manual deserialize needed
+//    @JsonProperty("points_encoded")
+//    val pointsEncoded: Boolean,
+//    val bbox: List<Double>,
+//    val instructions: List<GraphhopperRouteInstruction>,
+//    val details: Any?,
+//    @JsonProperty("points_order")
+//    val pointsOrder: List<Int>?,
+)
+
+data class GraphhopperRouteInstruction(
+    val text: String,
+    @JsonProperty("street_name")
+    val streetName: String?,
+    val distance: Double,
+    val time: Int,
+    @JsonProperty("interval")
+    val interval: List<Int>,
+    val sign: Int?,
+    @JsonProperty("exit_number")
+    val exitNumber: Int?,
+    @JsonProperty("turn_angle")
+    val turnAngle: Double
+)
+
+data class GraphhopperRouteResponseInfo(
+    val copyrights: List<String>,
+    val took: Int
+)
